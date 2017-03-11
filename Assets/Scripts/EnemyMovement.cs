7d6a097c232d617de2bcca381424970d789cf71f
Stella <puddinglaterne@googlemail.com>
2017-03-11 12:12:15 +0100

    DOTZ!

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,48 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour {
-
+public class EnemyMovement : MonoBehaviour
+{
     public Vector2 MoveDir;
     public float MoveSpeed;
+    public float TopAngle = 45;
+
     private Rigidbody2D rb;
     private FootScript fs;
+    private bool dotzCooldownActive;
+    private bool isDying;
+
     public float TimeTillDecay;
-	// Use this for initialization
-	void Start () {
+    public float DotzAngle = 45;
+    public float DotzForce = 5;
+    private float DotzCooldown = 1;
+
+	void Start ()
+    {
         rb = GetComponent<Rigidbody2D>();
-        fs = transform.GetChild(1).GetComponent<FootScript>();
+        fs = GetComponentInChildren<FootScript>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        if(fs.isGrounded())
+	void Update ()
+    {
+        if(fs.isGrounded() && !dotzCooldownActive && !isDying)
         {
             rb.velocity = new Vector2(MoveDir.x * MoveSpeed, rb.velocity.y);
-        }  
-        
+        }          
     }
+
     private IEnumerator WaitTillDeath()
     {
         yield return new WaitForSeconds(TimeTillDecay);
         gameObject.SetActive(false);
     }
+
     public void Squish()
     {
         transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y / 2, transform.localScale.z);
+        isDying = true;
         StartCoroutine(WaitTillDeath());
+    }
 
-    }
-    public void Dotz()
+    public void Dotz(Vector2 dotzOrigin)
     {
-        //TODO: Dotz hard
+        if (dotzCooldownActive) return;
+
+        Vector2 dotzDirection = Vector2.up;
+        if(dotzOrigin.x > transform.position.x)
+        {
+            dotzDirection = dotzDirection.Rotate(DotzAngle);
+        }
+        else
+        {
+            dotzDirection = dotzDirection.Rotate(-DotzAngle);
+        }
+        rb.AddForce(dotzDirection * DotzForce, ForceMode2D.Impulse);
+        StartCoroutine(WaitForDotzCooldown());
     }
+
+    private IEnumerator WaitForDotzCooldown()
+    {
+        dotzCooldownActive = true;
+        yield return new WaitForSeconds(DotzCooldown);
+        dotzCooldownActive = false;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.tag == "Food")
         {
             collision.gameObject.SetActive(false);
-        }else if(collision.collider.tag == "Rudi")
+        }
+        else if(collision.collider.tag == "Rudi")
         {
             this.gameObject.SetActive(false);
         }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,58 +6,59 @@ using UnityEngine.EventSystems;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private CircleCollider2D collider;
     public Collider2D groundLeftCollider;
     public Collider2D groundRightCollider;
-    private FootScript fs;
+    private FootScript footScript;
+
     private bool jumpPressed;
     private bool grounded;
+    private float horizontalMovement;
+
     public float Speed;
     public float JumpStrength;
     public float MinGroundDistance;
     public float RayCastOffset;
-    // Use this for initialization
+    public float MinDotzInput = 0.5f;
+
     void Start ()
 	{
 	    rb = GetComponent<Rigidbody2D>();
-	    collider = GetComponent<CircleCollider2D>();
-        fs = transform.GetChild(0).GetComponent<FootScript>();
-
+        footScript = transform.GetComponentInChildren<FootScript>();
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Jump") && fs.isGrounded())
+        if (Input.GetButtonDown("Jump") && footScript.isGrounded())
         {
             jumpPressed = true;
         }
     }
     private void FixedUpdate()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
+        horizontalMovement = Input.GetAxis("Horizontal");
 
-       //var rc =  Physics2D.Raycast(transform.position - (Vector3.down * RayCastOffset), Vector2.down,MinGroundDistance, LayerMask.NameToLayer("Default"));
-       // Debug.Log(rc.distance);
         if (jumpPressed)
         {
-            rb.AddForce(Vector2.up*JumpStrength,ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * JumpStrength, ForceMode2D.Impulse);
             jumpPressed = false;
         }
-        rb.velocity = new Vector2(horizontalMovement*Speed,rb.velocity.y);
+
+        rb.velocity = new Vector2(horizontalMovement * Speed, rb.velocity.y);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.collider.tag == "Enemy")
+        EnemyMovement enemy = collision.gameObject.GetComponentInChildren<EnemyMovement>();
+        if(enemy != null)
         {
             ContactPoint2D hitPoint = collision.contacts[0];
-           float angle = hitPoint.normal.GetAngle();
-            if(angle > -60 && angle < 60)
+            float angle = hitPoint.normal.GetAngle();
+            if(angle > 360 - enemy.TopAngle || angle < enemy.TopAngle)
             {
-                collision.gameObject.GetComponent<EnemyMovement>().Squish();
+                enemy.Squish();
             }
-            else
+            else if(Mathf.Abs(horizontalMovement) > MinDotzInput)
             {
-                //dotz
+                enemy.Dotz(transform.position);
             }
         }
     }
